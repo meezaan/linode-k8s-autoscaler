52a009d959f5adc2ba939f8d4ce886106487169a
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2021-04-15 00:14:28 +0400

    Fix tests

--- tests/Unit/ScaleTest.php
+++ tests/Unit/ScaleTest.php
@@ -12,23 +12,23 @@ class ScaleTest extends TestCase
      */
     public function testScale()
     {
-        $scale = new Scale(65, 30, 50, 60, 70, 55);
+        $scale = new Scale(65, 30, 50, 60, 70, 55, 'used');
         $this->assertFalse($scale->scaleDown());
         $this->assertFalse($scale->scaleUp());
 
-        $scale = new Scale(65, 30, 67, 60, 70, 60);
+        $scale = new Scale(65, 30, 67, 60, 70, 60, 'used');
         $this->assertFalse($scale->scaleDown());
         $this->assertTrue($scale->scaleUp());
 
-        $scale = new Scale(65, 30, 63, 70, 60, 50);
+        $scale = new Scale(65, 30, 63, 70, 60, 50, 'requested');
         $this->assertFalse($scale->scaleDown());
         $this->assertTrue($scale->scaleUp());
 
-        $scale = new Scale(65, 30, 29, 70, 80, 75);
+        $scale = new Scale(65, 30, 29, 70, 80, 75, 'requested');
         $this->assertTrue($scale->scaleDown());
         $this->assertFalse($scale->scaleUp());
 
-        $scale = new Scale(65, 30, 40, 70, 80, 75);
+        $scale = new Scale(65, 30, 40, 70, 80, 75, 'requested');
         $this->assertTrue($scale->scaleDown());
         $this->assertFalse($scale->scaleUp());
 
